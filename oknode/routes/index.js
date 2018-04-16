c65db01b3942de2ce8c16afcbbf59e2e9a7742d2
kenu <kenu.heo@gmail.com>
2018-04-17 01:18:51 +0900

    4000

--- oknode/routes/index.js
+++ oknode/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'OKKY node.js version' });
 });
 
 module.exports = router;
